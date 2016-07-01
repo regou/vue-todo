71bb9d7dc25ca47cf099288a148de0ac61fd5b6d
wangxiao <wangxiao@patsnap.com>
2016-07-01 15:25:50 +0800

    voice init

---
+++ src/components/Header/voice.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="add-voice" @click="record">
+        <i class="fa fa-plus"></i>
+    </div>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+    import HeaderComponent from './components/header.vue'
+    import OtherComponent from './components/other.vue'
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+            'other-component':OtherComponent,
+            HeaderComponent,
+        }
+    }
+</script>
